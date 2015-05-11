2bb4212641705b5f315c34dbdb610f6a4998ed20
Rhoda Jiao <r.jiao@utexas.edu>
2015-05-11 12:21:11 -0500

    rmd

--- 03 Visualizations/crosstabs.R
+++ 03 Visualizations/crosstabs.R
@@ -1,3 +1,4 @@
+print("The first crosstab displays the count of HDI categories by continent. African countries mostly have low HDIs and European countries have mostly high HDIs. This crosstab was created by using a cube operator.")
 hditabs <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select
 CASE
@@ -10,15 +11,21 @@ from continents c join hdi h on(c.country=h.country) group by cube (continent, h
 hditabs <- filter(hditabs, HDICATEGORY != "null") 
 hdiorder = c("Low", "Medium", "High", "VeryHigh", "Total")
 hditabs$HDICATEGORY <- ordered(hditabs$HDICATEGORY, hdiorder)
-hditabs %>% arrange() %>% spread(HDICATEGORY, N)
+print(spread(hditabs, HDICATEGORY, N))
 
+print("")
+
+print("This crosstab shows the difference in mortality from stomach cancer in men relative to the lowest mortality in that continent. This crosstab used a subquery to find the minimum mortality and then computed the difference.")
 stomach_mortality <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT country, continent, stomach_male_mortality, mortality_min, stomach_male_mortality - mortality_min as mortality_diff, mortality_rank FROM(SELECT c.country, continent, stomach_male_mortality, min(stomach_male_mortality) 
 OVER (PARTITION BY continent) as MORTALITY_MIN, rank() 
 OVER (PARTITION BY continent order by stomach_male_mortality desc) as MORTALITY_RANK 
 FROM continents c join stomach_male_mortality cr on(c.country=cr.country) where year=\\\'2002\\\')"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(stomach_mortality)
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+print(head(stomach_mortality))
+print("")
 
+print("This crosstab shows the minimum life expectancy (years) for each country in the time span between 1950 and 2013. Most countries had the shortest life span in the year 1950, but there are a few that showed short life expectancies in later years. \nThis crosstab was created using a subquery to determine the minimum life expectancy partitioned by country and then matching that life expectancy to the year. \nThis crosstab is visualized in the following graph. Filters were used so that only those countries with minimum life expectancies after 1955 are labeled individually.")
 minlifeexpectancy <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT country, year, life_expectancy as min_life_expectancy FROM (SELECT country, year, life_expectancy, min(life_expectancy) OVER(PARTITION BY country) AS min_life_expectancy FROM life_expectancy) WHERE life_expectancy = min_life_expectancy ORDER BY 2,3"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(minlifeexpectancy)
--- 03 Visualizations/viz.R
+++ 03 Visualizations/viz.R
@@ -1,38 +1,14 @@
-print(ggplot(MaleFemaleLiverMortalityPlot, aes(x=COUNTRY,y=LIVER_MALE_MORTALITY/LIVER_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male to Female Liver Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,8,1))+ggtitle('Male to Female Liver Cancer Deaths Across the World'))
+print("These three visualizations show scatterplots of the male to female mortality of specific types of cancer. Each is facet wrapped by continent.")
+print(ggplot(MaleFemaleLiverMortalityPlot, aes(x=COUNTRY,y=LIVER_MALE_MORTALITY/LIVER_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male/Female Liver Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,8,1))+ggtitle('Male to Female Liver Cancer Deaths Across the World'))
 
-print(ggplot(MaleFemaleLungMortalityPlot, aes(x=COUNTRY,y=LUNG_MALE_MORTALITY/LUNG_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male to Female Lung Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,30,1))+ggtitle('Male to Female Lung Cancer Deaths Across the World'))
+print(ggplot(MaleFemaleLungMortalityPlot, aes(x=COUNTRY,y=LUNG_MALE_MORTALITY/LUNG_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male/Female Lung Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,30,1))+ggtitle('Male to Female Lung Cancer Deaths Across the World'))
 
-print(ggplot(MaleFemaleColonMortalityPlot, aes(x=COUNTRY,y=COLON_MALE_MORTALITY/COLON_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male to Female Colon Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,4,1))+ggtitle('Male to Female Colon Cancer Deaths Across the World'))
+print(ggplot(MaleFemaleColonMortalityPlot, aes(x=COUNTRY,y=COLON_MALE_MORTALITY/COLON_FEMALE_MORTALITY))+geom_point()+ facet_wrap(~CONTINENT)+theme(axis.text.x=element_blank())+ylab('Ratio of Male/Female Colon Cancer Deaths Per 100,000 People')+scale_y_continuous(breaks=seq(0,4,1))+ggtitle('Male to Female Colon Cancer Deaths Across the World'))
+print("")
 
-print(ggplot(MortalIncidence, aes(x=HDI_NUMBER, y=Mortality, color=CONTINENT)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(4,2)) + facet_wrap(~TYPE) + ggtitle("Mortality by HDI Level (2002)") + labs(x="HDI Number", y="Mortality (per 100,000 people)") +theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))) 
+print("The following three visualizations are based on a data frame derived from the joined cancer data set that was reshaped so that the data were grouped by country and type with values of mortality and incidence. The visualizations show mortality (deaths per 100,000 people), incidence (new cases per 100,000 people), and the mortality / incidence ratio respectively, facet wrapped for type of cancer. These variables were scaled by HDI number.")
+print(ggplot(MortalIncidence, aes(x=HDI_NUMBER, y=Mortality, color=CONTINENT)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(2,1)) + facet_wrap(~TYPE) + ggtitle("Mortality by HDI Level (2002)") + labs(x="HDI Number", y="Mortality (per 100,000 people)") +theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))) 
 
-print(ggplot(MortalIncidence, aes(x=HDI_NUMBER, y=Incidence, color=CONTINENT)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(4,2)) + facet_wrap(~TYPE) + ggtitle("Incidence by HDI Level (2002)") + labs(x="HDI Number", y="Incidence (per 100,000 people)") +theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))) 
+print(ggplot(MortalIncidence, aes(x=HDI_NUMBER, y=Incidence, color=CONTINENT)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(2,1)) + facet_wrap(~TYPE) + ggtitle("Incidence by HDI Level (2002)") + labs(x="HDI Number", y="Incidence (per 100,000 people)") +theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))) 
 
-# # Male Rates
-# print(ggplot(aggData, aes(x=HDINUMBER,y=LIVER_MALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Male Liver Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=COLON_MALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Male Colon Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=LUNG_MALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Male Lung Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=PROSTATE_MALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Male Prostate Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=STOMACH_MALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Male Stomach Cancer Mortality per 100,000 people'))
-# 
-# 
-# # Female Rates
-# print(ggplot(aggData, aes(x=HDINUMBER,y=BREAST_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Breast Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=CERVIX_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Cervix Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=LIVER_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Liver Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=COLON_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Colon Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=LUNG_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Lung Cancer Mortality per 100,000 people'))
-# 
-# print(ggplot(aggData, aes(x=HDINUMBER,y=STOMACH_FEMALE_MORTALITY,color=CONTINENT))+geom_point()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+ylab('Female Stomach Cancer Mortality per 100,000 people'))
-
-## mortality to incidence ratio
-
-print(ggplot(MortalIncidence, aes(x=Incidence, y=Mortality, color=HDI_NUMBER)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(4,2)) + facet_wrap(~TYPE) + xlim(0,125) + ylim(0,100) + ggtitle("Mortality to Incidence Ratio by HDI Level (2002)") + labs(x="Incidence (per 100,000 people)", y="Mortality (per 100,000 people)"))
+print(ggplot(MortalIncidence, aes(x=Incidence, y=Mortality, color=HDI_NUMBER)) + geom_point(aes(size=LIFE_EXPECTANCY)) + scale_size_continuous(range=c(2,1)) + facet_wrap(~TYPE) + xlim(0,125) + ylim(0,100) + ggtitle("Mortality to Incidence Ratio by HDI Level (2002)") + labs(x="Incidence (per 100,000 people)", y="Mortality (per 100,000 people)"))
