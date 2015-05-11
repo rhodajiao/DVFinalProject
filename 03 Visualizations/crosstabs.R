0952a20ed7f4e6f111ea93895ab09855df88c3e2
Rhoda Jiao <r.jiao@utexas.edu>
2015-05-11 11:33:02 -0500

    min life expectancy

--- 03 Visualizations/crosstabs.R
+++ 03 Visualizations/crosstabs.R
@@ -19,3 +19,12 @@ OVER (PARTITION BY continent order by stomach_male_mortality desc) as MORTALITY_
 FROM continents c join stomach_male_mortality cr on(c.country=cr.country) where year=\\\'2002\\\')"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(stomach_mortality)
 
+minlifeexpectancy <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"SELECT country, year, life_expectancy as min_life_expectancy FROM (SELECT country, year, life_expectancy, min(life_expectancy) OVER(PARTITION BY country) AS min_life_expectancy FROM life_expectancy) WHERE life_expectancy = min_life_expectancy ORDER BY 2,3"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(minlifeexpectancy)
+
+minlifeexpectancy1 <- minlifeexpectancy %>% filter(YEAR > 1955) %>% mutate(label = COUNTRY)
+minlifeexpectancy2 <- minlifeexpectancy %>% filter(YEAR <= 1955) %>% mutate(label = NULL) 
+minlifeexpectancy <- bind_rows(minlifeexpectancy1, minlifeexpectancy2)
+
+print(ggplot(minlifeexpectancy, aes(x=YEAR, y=MIN_LIFE_EXPECTANCY)) + geom_point() + geom_text(label=minlifeexpectancy$label, angle = 45, size = 4, hjust = 0, vjust = 0) + ylim(15,90) + xlim(1950,2015) + ggtitle("Minimum Life Expectancy By Country (1950-2013)") + labs(x="Year", y="Minimum Life Expectancy (Years)"))
